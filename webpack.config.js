a8eb2d38e57a0c2f05b2cb517e79b204fb12fc26
Dima <strongbaby1337@gmail.com>
2021-09-02 13:33:38 +0300

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const { VueLoaderPlugin } = require("vue-loader")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/js/popup.js",
+    output: {
+        path: path.resolve("dist"),
+        filename: "[name].[contenthash].js"
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            "@": path.resolve("src"),
+            "@components": path.resolve("src", "js", "components"),
+        },
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    devServer: {
+        port: 5500
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "popup.html",
+            template: path.resolve("public", "popup.html")
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve("public", "manifest.json"),
+                    to: path.resolve("dist")
+                },
+                {
+                    from: path.resolve("public", "icons"),
+                    to: path.resolve("dist", "icons")
+                }
+            ]
+        }),
+        new VueLoaderPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            }
+        ]
+    }
+}
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-
-
-require("laravel-mix")
-  .setPublicPath("./")
-  .js("src/js/popup.js", "dist/js").vue()
