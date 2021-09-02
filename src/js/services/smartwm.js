9e67c9f2711eb12cb9eb8f46686bb0dcd1090695
Dima <strongbaby1337@gmail.com>
2021-09-02 14:45:12 +0300

    upd indentation

--- src/js/services/smartwm.js
+++ src/js/services/smartwm.js
@@ -1,11 +1,11 @@
 export default async function fetchFromNetlify(data) {
-    const url = "https://modest-shaw-b5cb04.netlify.app/.netlify/functions/parsePrice"
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data),
-    });
-    return await response.json();
+  const url = "https://modest-shaw-b5cb04.netlify.app/.netlify/functions/parsePrice"
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,53 +5,53 @@ const CopyWebpackPlugin = require("copy-webpack-plugin")
 const { VueLoaderPlugin } = require("vue-loader")
 
 module.exports = {
-    mode: "development",
-    entry: "./src/js/popup.js",
-    output: {
-        path: path.resolve("dist"),
-        filename: "[name].[contenthash].js"
+  mode: "development",
+  entry: "./src/js/popup.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "[name].[contenthash].js"
+  },
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      "@": path.resolve("src"),
+      "@components": path.resolve("src", "js", "components"),
     },
-    resolve: {
-        extensions: [".js"],
-        alias: {
-            "@": path.resolve("src"),
-            "@components": path.resolve("src", "js", "components"),
-        },
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        }
-    },
-    devServer: {
-        port: 5500
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "popup.html",
-            template: path.resolve("public", "popup.html")
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve("public", "manifest.json"),
-                    to: path.resolve("dist")
-                },
-                {
-                    from: path.resolve("public", "icons"),
-                    to: path.resolve("dist", "icons")
-                }
-            ]
-        }),
-        new VueLoaderPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: "vue-loader",
-            }
-        ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
     }
+  },
+  devServer: {
+    port: 5500
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "popup.html",
+      template: path.resolve("public", "popup.html")
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve("public", "manifest.json"),
+          to: path.resolve("dist")
+        },
+        {
+          from: path.resolve("public", "icons"),
+          to: path.resolve("dist", "icons")
+        }
+      ]
+    }),
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      }
+    ]
+  }
 }
