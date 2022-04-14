76233a7f2e7a3ceefbe2be4090d6d13f8f4ff92a
Dima <strongbaby1337@gmail.com>
2022-04-15 01:36:00 +0300

    Update Popup.vue

--- src/js/components/Popup.vue
+++ src/js/components/Popup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" :style="isExtension ? 'width: 500px' : ''">
     <PasswordInput />
     <SmartWM />
   </div>
@@ -12,6 +12,20 @@ import SmartWM from "./SmartWM.vue";
 export default {
   components: {
     PasswordInput, SmartWM
+  },
+  data() {
+    return {
+      isExtension: false,
+    }
+  },
+  mounted() {
+    // ! kinda easiest way
+    // if (window?.chrome?.extension) {
+    //   this.isExtension = true
+    // }
+
+    // ! manually configured webpack
+    this.isExtension = process.env.BUILD_ENV === "extension"
   }
 }
 </script>
